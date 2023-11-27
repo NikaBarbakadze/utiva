d7a9e11f3d316b37d28ddbd4891c9d231d6edcfd
NikaBarbakadze <45419134+NikaBarbakadze@users.noreply.github.com>
2023-11-28 04:06:42 +1100

    stickets

--- assets/play-video.js
+++ assets/play-video.js
@@ -1,1 +1,33 @@
-(()=>{document.addEventListener("DOMContentLoaded",function(){let d=document.querySelector(".customer-feedback__block"),t=d.querySelector(".customer-feedback__block--play"),e=d.querySelector("video");t.addEventListener("click",()=>{e.paused&&(e.play(),t.style.display="none")}),e.addEventListener("click",()=>{e.paused||(e.pause(),t.style.display="block")}),e.addEventListener("ended",()=>{t.style.display="block"})});})();
+(() => {
+  document.addEventListener("DOMContentLoaded", function () {
+    // Select all instances of .customer-feedback__block
+    let blocks = document.querySelectorAll(".customer-feedback__block");
+
+    // Iterate over each block
+    blocks.forEach((d) => {
+      let t = d.querySelector(".customer-feedback__block--play");
+      let e = d.querySelector("video");
+
+      // Event listener for play button
+      t.addEventListener("click", () => {
+        if (e.paused) {
+          e.play();
+          t.style.display = "none";
+        }
+      });
+
+      // Event listener for video element
+      e.addEventListener("click", () => {
+        if (!e.paused) {
+          e.pause();
+          t.style.display = "block";
+        }
+      });
+
+      // Event listener for when the video ends
+      e.addEventListener("ended", () => {
+        t.style.display = "block";
+      });
+    });
+  });
+})();
