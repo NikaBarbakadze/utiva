4d4aaf585c54c7a1ce60c839df490b761d9cb5e9
NikaBarbakadze <45419134+NikaBarbakadze@users.noreply.github.com>
2023-12-04 12:13:30 +0400

    language

--- assets/toggle.js
+++ assets/toggle.js
@@ -1,60 +1,16 @@
-(() => {
-  function a(s, g, r, o, c) {
-    let toggles = document.querySelectorAll("#" + s);
-    let beforeElems = document.querySelectorAll(g);
-    let afterElems = document.querySelectorAll(r);
+document.addEventListener("DOMContentLoaded", function () {
+  var checkboxes = document.querySelectorAll(".header-desk__toggle");
 
-    toggles.forEach((toggle, index) => {
-      let e = beforeElems[index];
-      let t = afterElems[index];
-
-      toggle.addEventListener("click", function () {
-        if (toggle.checked) {
-          e.style.color = o;
-          e.classList.add("active-toggle");
-          e.classList.remove("unactive-toggle");
-          t.style.color = c;
-          t.classList.add("unactive-toggle");
-          t.classList.remove("active-toggle");
-        } else {
-          e.style.color = c;
-          e.classList.add("unactive-toggle");
-          e.classList.remove("active-toggle");
-          t.style.color = o;
-          t.classList.add("active-toggle");
-          t.classList.remove("unactive-toggle");
-        }
-      });
-    });
-  }
-
-  document.addEventListener("DOMContentLoaded", function () {
-    a(
-      "mobile-menu__toggle",
-      ".mobile-menu__toggle--before",
-      ".mobile-menu__toggle--after",
-      "var(--color-black-60)",
-      "var(--color-black)"
-    );
-
-    a(
-      "header-desk__toggle",
-      ".header-desktop__icons--toggle-track--before",
-      ".header-desktop__icons--toggle-track--after",
-      "var(--color-white-60)",
-      "var(--color-white)"
-    );
-
-    var checkboxes = document.querySelectorAll(".header-desk__toggle");
-
-    checkboxes.forEach((checkbox) => {
-      checkbox.addEventListener("change", function () {
-        if (checkbox.checked) {
-          window.location.href = "https://fr.utivahealth.ca";
-        } else {
-          window.location.href = "https://utivahealth.ca";
-        }
-      });
+  checkboxes.forEach((checkbox) => {
+    if (window.location.href.includes("fr.")) {
+      checkbox.checked = true;
+    }
+    checkbox.addEventListener("change", function () {
+      if (checkbox.checked === true) {
+        window.location.href = "https://fr.utivahealth.ca";
+      } else {
+        window.location.href = "https://utivahealth.ca";
+      }
     });
   });
-})();
+});
