cbfe520f2c967891404b50dcee5414fc6b1055a4
NikaBarbakadze <45419134+NikaBarbakadze@users.noreply.github.com>
2023-12-29 03:56:41 +0400

    updates

--- assets/toggle.js
+++ assets/toggle.js
@@ -10,16 +10,16 @@ document.addEventListener("DOMContentLoaded", function () {
       let path = window.location.pathname;
       let queryString = window.location.search;
 
-      // Determine the base domain
-      let baseDomain = "utivahealth.ca";
-      if (window.location.host.includes("www.")) {
-        baseDomain = "www.utivahealth.ca";
+      // Determine the base domain, removing 'www.' if present
+      let baseDomain = window.location.host.replace("www.", "");
+      if (!baseDomain.includes("utivahealth.ca")) {
+        baseDomain = "utivahealth.ca"; // Fallback to the main domain if needed
       }
 
       // Construct the new URL based on the checkbox state
       let newUrl = checkbox.checked
         ? "https://fr." + baseDomain + path + queryString
-        : "https://" + baseDomain.replace("www.", "") + path + queryString;
+        : "https://" + baseDomain + path + queryString;
 
       // Redirect to the new URL
       window.location.href = newUrl;
