f81835747f73c7835c8a1d5cf07067d72887b887
NikaBarbakadze <45419134+NikaBarbakadze@users.noreply.github.com>
2023-12-29 03:59:53 +0400

    updates

--- assets/toggle.js
+++ assets/toggle.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var checkboxes = document.querySelectorAll(".header-desk__toggle");
 
   checkboxes.forEach((checkbox) => {
-    // Initialize the checkbox based on current domain
+    // Set the checkbox based on the current domain
     checkbox.checked = window.location.host.startsWith("fr.");
 
     checkbox.addEventListener("change", function () {
@@ -10,14 +10,21 @@ document.addEventListener("DOMContentLoaded", function () {
       let path = window.location.pathname;
       let queryString = window.location.search;
 
-      // Construct the new URL based on the checkbox state
-      let newUrl;
+      // Remove 'www.' if present and construct the new URL
+      let hostWithoutWww = window.location.host.replace("www.", "");
+      let newUrl = "";
+
       if (checkbox.checked) {
         // Redirect to the French subdomain
-        newUrl = "https://fr.utivahealth.ca" + path + queryString;
+        newUrl =
+          "https://fr." +
+          hostWithoutWww.replace("fr.", "") +
+          path +
+          queryString;
       } else {
         // Redirect to the regular domain
-        newUrl = "https://utivahealth.ca" + path + queryString;
+        newUrl =
+          "https://" + hostWithoutWww.replace("fr.", "") + path + queryString;
       }
 
       // Redirect to the new URL
