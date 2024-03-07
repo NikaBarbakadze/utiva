085515ae217e9d8cc4bcd1fe85cddcad774c30b6
NikaBarbakadze <45419134+NikaBarbakadze@users.noreply.github.com>
2024-03-07 15:35:20 +0400

    subscription fix

--- assets/header-height.js
+++ assets/header-height.js
@@ -1,1 +1,22 @@
-(()=>{window.addEventListener("DOMContentLoaded",()=>{let e=document.getElementById("announcement-bar"),n=document.getElementById("shopify-section-header");console.log("header",n),console.log("announcementBar",e);let s={"announcement-bar":"--announcement-bar-height","shopify-section-header":"--header-height"},t=new ResizeObserver(c);e&&t.observe(e),n&&t.observe(n);function c(a){for(let o of a){let r=s[o.target.id];r&&document.documentElement.style.setProperty(r,`${o.contentRect.height}px`)}}});})();
+(() => {
+  window.addEventListener("DOMContentLoaded", () => {
+    let e = document.getElementById("announcement-bar"),
+      n = document.getElementById("shopify-section-header");
+    let s = {
+        "announcement-bar": "--announcement-bar-height",
+        "shopify-section-header": "--header-height",
+      },
+      t = new ResizeObserver(c);
+    e && t.observe(e), n && t.observe(n);
+    function c(a) {
+      for (let o of a) {
+        let r = s[o.target.id];
+        r &&
+          document.documentElement.style.setProperty(
+            r,
+            `${o.contentRect.height}px`
+          );
+      }
+    }
+  });
+})();
